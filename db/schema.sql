33c9975f5f123252b500b575ce233915c201b464
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-12-08 19:11:05 -0600

    db fix

--- db/schema.sql
+++ db/schema.sql
@@ -2,19 +2,4 @@ DROP DATABASE IF EXISTS IPAPI;
 
 CREATE DATABASE IPAPI;
 
--- CREATE TABLE user (
---     username VARCHAR(30) UNIQUE NOT NULL PRIMARY KEY,
---     first_name VARCHAR(30) NOT NULL,
---     age INT NOT NULL,
---     email VARCHAR(40) UNIQUE NOT NULL,
---     beer_type VARCHAR(30) NOT NULL,
---     password 
-
--- )
--- CREATE TABLE beer (
---     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
---     beer_name VARCHAR(30) UNIQUE NOT NULL,
---     brewery VARCHAR(30) NOT NULL,
---     beer_type VARCHAR(30) NOT NULL
-
--- )
+SELECT * FROM Users;
