c596174804ca225d09f414d051ab86b69f6e105a
parappa20 <88298540+james182-dang@users.noreply.github.com>
2022-01-10 21:12:41 -0600

    connection update

--- config/connection.js
+++ config/connection.js
@@ -14,16 +14,42 @@ require('dotenv').config();
 
 
 
-let sequelize;
+// let sequelize;
 
+// if (process.env.JAWSDB_URL) {
+//     sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         port: 3306
+//     });
+// }
+
+var mysql = require("mysql");
+var connection;
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    //change back to "password"
+    password: "password",
+    database: "petPartyDB"
+  });
 }
+//console.log(process.env.JAWSDB_URL);
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  //console.log("connected as id " + connection.threadId);
+});
+module.exports = connection;
 
-module.exports = sequelize;
+
+
+// module.exports = sequelize;
